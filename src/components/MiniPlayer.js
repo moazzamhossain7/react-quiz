fca0aab7a22ce84cc9b7685ca09b77e66f1758ce
moazzamhossain7 <en.moazzam@gmail.com>
2022-01-13 12:20:04 +0600

    setup miniplayer & progress tooltip

--- src/components/MiniPlayer.js
+++ src/components/MiniPlayer.js
@@ -1,18 +1,47 @@
-import image from "../assets/images/3.jpg";
+import { useRef, useState } from "react";
+import ReactPlayer from "react-player/youtube";
 import classes from "../styles/MiniPlayer.module.css";
 
-export default function MiniPlayer() {
+export default function MiniPlayer({ id, title }) {
+    const videoUrl = `https://www.youtube.com/watch?v=${id}`;
+    const buttonRef = useRef();
+    const [status, setStatus] = useState(false);
+
+    function toogleMiniPlayer() {
+        if (status) {
+            setStatus(false);
+            buttonRef.current.classList.add(classes.floatingBtn);
+        } else {
+            setStatus(true);
+            buttonRef.current.classList.remove(classes.floatingBtn);
+        }
+    }
+
     return (
-        <div className={`${classes.miniPlayer} ${classes.floatingBtn}`}>
+        <div
+            className={`${classes.miniPlayer} ${classes.floatingBtn}`}
+            ref={buttonRef}
+            onClick={toogleMiniPlayer}
+        >
             <span className={`material-icons-outlined ${classes.open}`}>
                 play_circle_filled
             </span>
-            <span className={`material-icons-outlined ${classes.close}`}>
+            <span
+                className={`material-icons-outlined ${classes.close}`}
+                onClick={toogleMiniPlayer}
+            >
                 {" "}
                 close{" "}
             </span>
-            <img src={image} alt="" />
-            <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+            <ReactPlayer
+                className={classes.player}
+                playing={status}
+                controls
+                url={videoUrl}
+                width="300px"
+                height="168px"
+            />
+            <p>{title}</p>
         </div>
     );
 }
